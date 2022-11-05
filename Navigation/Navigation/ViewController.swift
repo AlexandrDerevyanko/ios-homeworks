ea05fb2a4fac258590c9137eff580f29cadb48a7
Alexandr Derevyanko <7171sites@gmail.com>
2022-11-06 02:54:09 +0800

    first commit

---
+++ Navigation/Navigation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Aleksandr Derevyanko on 05.11.2022.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
