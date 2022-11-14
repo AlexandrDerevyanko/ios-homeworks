30d3f0c7f2399c39e0d3387aac7e275f32451d62
Alexandr Derevyanko <7171sites@gmail.com>
2022-11-14 16:43:08 +0800

    some commit

--- Navigation/Navigation/ViewController.swift
+++ Navigation/Navigation/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .cyan
-        // Do any additional setup after loading the view.
     }
 
 
