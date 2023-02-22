c90bd2714647d45639973fd87a07006f432dd2ff
Alexandr Derevyanko <7171sites@gmail.com>
2023-02-22 16:58:11 +0600

    some commit

--- Navigation/Navigation/Factory/AppFactory.swift
+++ Navigation/Navigation/Factory/AppFactory.swift
@@ -17,10 +17,13 @@ final class AppFactory {
             let view = UINavigationController(rootViewController: FeedViewController(viewModel: viewModel))
             return Module(moduleType: moduleType, viewModel: viewModel, view: view)
         case .profile:
-            let logInVC = LogInViewController()
-            logInVC.loginDelegate = MyLoginFactory().makeLoginInspector()
-            let view = UINavigationController(rootViewController: logInVC)
-            return Module(moduleType: moduleType, viewModel: nil, view: view)
+            let viewModel = ProfileViewModel()
+            let view = UINavigationController(rootViewController: ProfileViewController(viewModel: viewModel, user: User(logIn: "Corgi", fullName: "Corgi Kevin", avatar: UIImage(named: "1") ?? UIImage(), status: "I'm fine")))
+            return Module(moduleType: moduleType, viewModel: viewModel, view: view)
+//            let logInVC = LogInViewController()
+//            logInVC.loginDelegate = MyLoginFactory().makeLoginInspector()
+//            let view = UINavigationController(rootViewController: logInVC)
+//            return Module(moduleType: moduleType, viewModel: nil, view: view)
         }
     }
 }
